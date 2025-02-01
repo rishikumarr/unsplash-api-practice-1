a776348804b26f66809138bffe2abc7f9c5e786b
rishikumarr <rishikumaarrbtech@gmail.com>
2025-02-01 15:12:46 +0530

    Created a context for photos and implemented pagination functionality too

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@ import Header from './components/Header';
 import HeroSection from './components/HeroSection';
 import MainContainer from './components/MainContainer';
 import TaskBar from './components/TaskBar';
+import { PhotosProvider } from './contexts/PhotosContext';
 
 function App() {
   return (
     <div className='w-full min-h-svh bg-slate-300'>
-      <div className='max-w-7xl min-h-svh border-l border-r border-slate-400 shadow-xl bg-slate-200 mx-auto my-0'>
-        <Header/>
-        <HeroSection/>
-        <div className='mt-10 flex flex-col'>
-          <TaskBar/>
-          <MainContainer/>
+      <PhotosProvider>
+        <div className='max-w-7xl min-h-svh border-l border-r border-slate-400 shadow-xl bg-slate-200 mx-auto my-0'>
+          <Header/>
+          <HeroSection/>
+          <div className='mt-10 flex flex-col'>
+            <TaskBar/>
+            <MainContainer/>
+          </div>
         </div>
-      </div>
+      </PhotosProvider>
     </div>
   )
 }
--- src/contexts/usePhotos.tsx
+++ src/contexts/usePhotos.tsx
@@ -1,110 +1,121 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
-interface Photo {
-    src: {
-        original: string;
-    },
-    avg_color: string,
-    alt: string
-}
+// interface Photo {
+//     src: {
+//         original: string;
+//     },
+//     avg_color: string,
+//     alt: string
+// }
+
+// const usePhotos = () => {
+//     const clientId = import.meta.env.VITE_CLIENT_ID;
+//     const endPoint = 'https://api.pexels.com/v1/';
+
+//     const [photos, setPhotos] = useState<Photo[]>([]);
+//     const [photosLoading, setPhotosLoading] = useState<boolean>(true);
+//     const [randomPhoto, setRandomPhoto] = useState<object | null>(null);
+//     const [randomPhotoLoading, setRandomPhotoLoading] = useState<boolean>(true);
+//     const [page, setPage] = useState<number>(1);
+
+//     const goToNxtPage = () => setPage(prevPage => prevPage + 1);
+//     const goToPrevPage = () => { if (page > 1) setPage(prevPage => prevPage - 1) };
+//     const goToPage = (page: number) => setPage(page);
+
+//     useEffect(() => {
+//         fetchImages('', page);
+//     }, [page]);
+
+//     const buildURL = (baseURL: string, params: Record<string, string | number>) => {
+//         const url = new URL(baseURL);
+//         const searchParams = new URLSearchParams(params as Record<string, string>);
+//         url.search = searchParams.toString();
+//         return url.toString();
+//     };
+
+//     const fetchImages = async (query: string = '', page: number = 1, perPage: number = 10) => {
+//         const params = {
+//             page,
+//             per_page: perPage,
+//         };
+
+//         if (query) params.query = query;
+
+//         const url = buildURL(`${endPoint}curated`, params);
+
+//         setPhotosLoading(true);
+
+//         try {
+//             const response = await fetch(url, {
+//                 headers: {
+//                     Authorization: clientId,
+//                 }
+//             });
+
+//             if (response.ok) {
+//                 const data = await response.json();
+//                 // console.log(data.photos);
+//                 setPhotos(data.photos);
+//                 // console.log(photos);
+//                 setPhotosLoading(false);
+//             }
+//         } catch (error) {
+//             console.log(error);
+//             setPhotosLoading(false);
+//         }
+//     };
+
+//     const fetchRandomImage = async () => {
+//         setRandomPhotoLoading(true);
+
+//         try {
+//             const response = await fetch(`${endPoint}curated`, {
+//                 headers: {
+//                     Authorization: clientId,
+//                 }
+//             });
+
+//             if (response.ok) {
+//                 const data = await response.json();
+
+//                 const photos = data.photos;
+
+//                 if (photos.length > 0) {
+//                     const randomIndex = Math.floor(Math.random() * photos.length);
+
+//                     setRandomPhoto(photos[randomIndex]);
+//                     setRandomPhotoLoading(false);
+//                 }
+//             }
+//         } catch (error) {
+//             console.log(error);
+//             setRandomPhotoLoading(true);
+//         }
+//     };
+
+//     return {
+//         photos,
+//         randomPhoto,
+//         fetchImages,
+//         fetchRandomImage,
+//         photosLoading,
+//         randomPhotoLoading,
+//         page,
+//         goToNxtPage,
+//         goToPrevPage,
+//         goToPage
+//     };
+// };
+
+// export default usePhotos;
+
+import { useContext } from "react";
+import { PhotosContext } from "./PhotosContext";
 
 const usePhotos = () => {
-    const clientId = import.meta.env.VITE_CLIENT_ID;
-    const endPoint = 'https://api.pexels.com/v1/';
+    const context = useContext(PhotosContext);
 
-    const [photos, setPhotos] = useState<Photo[]>([]);
-    const [photosLoading, setPhotosLoading] = useState<boolean>(true);
-    const [randomPhoto, setRandomPhoto] = useState<object | null>(null);
-    const [randomPhotoLoading, setRandomPhotoLoading] = useState<boolean>(true);
-    const [page, setPage] = useState<number>(1);
-
-    const goToNxtPage = () => setPage(prevPage => prevPage + 1);
-    const goToPrevPage = () => { if (page > 1) setPage(prevPage => prevPage - 1) };
-    const goToPage = (page: number) => setPage(page);
-
-    useEffect(() => {
-        fetchImages('', page);
-    }, [page]);
-
-    const buildURL = (baseURL: string, params: Record<string, string | number>) => {
-        const url = new URL(baseURL);
-        const searchParams = new URLSearchParams(params as Record<string, string>);
-        url.search = searchParams.toString();
-        return url.toString();
-    };
-
-    const fetchImages = async (query: string = '', page: number = 1, perPage: number = 10) => {
-        const params = {
-            page,
-            per_page: perPage,
-        };
-
-        if (query) params.query = query;
-
-        const url = buildURL(`${endPoint}curated`, params);
-
-        setPhotosLoading(true);
-
-        try {
-            const response = await fetch(url, {
-                headers: {
-                    Authorization: clientId,
-                }
-            });
-
-            if (response.ok) {
-                const data = await response.json();
-                // console.log(data.photos);
-                setPhotos(data.photos);
-                // console.log(photos);
-                setPhotosLoading(false);
-            }
-        } catch (error) {
-            console.log(error);
-            setPhotosLoading(false);
-        }
-    };
-
-    const fetchRandomImage = async () => {
-        setRandomPhotoLoading(true);
-
-        try {
-            const response = await fetch(`${endPoint}curated`, {
-                headers: {
-                    Authorization: clientId,
-                }
-            });
-
-            if (response.ok) {
-                const data = await response.json();
-
-                const photos = data.photos;
-
-                if (photos.length > 0) {
-                    const randomIndex = Math.floor(Math.random() * photos.length);
-
-                    setRandomPhoto(photos[randomIndex]);
-                    setRandomPhotoLoading(false);
-                }
-            }
-        } catch (error) {
-            console.log(error);
-            setRandomPhotoLoading(true);
-        }
-    };
-
-    return {
-        photos,
-        randomPhoto,
-        fetchImages,
-        fetchRandomImage,
-        photosLoading,
-        randomPhotoLoading,
-        page,
-        goToNxtPage,
-        goToPrevPage,
-        goToPage
-    };
-};
+    return context;
+}
 
 export default usePhotos;
